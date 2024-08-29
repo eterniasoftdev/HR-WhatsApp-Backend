0a24fdce168d1a9753f748c074a2e062d84d0727
Pawan608 <agarwalpawan6089@gmail.com>
2024-08-29 10:39:07 +0530

    logs

--- route/socketRoute.js
+++ route/socketRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 router.post("/", (req, res) => {
+  console.log("Request Body...", req);
   res.status(200).send({
     message: "success",
   });
